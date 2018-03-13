83390ac6a9b7bbbe8b1620e2f734266d75c2fa41
Gaurav Naik <gvrvnk@gmail.com>
2018-03-13 12:26:54 +0530

    Physician and patient after insert

--- idr_erpnext/api.py
+++ idr_erpnext/api.py
@@ -27,7 +27,6 @@ def generate_codice_fiscale(last_name, first_name, date_of_birth, gender, munici
 
 @frappe.whitelist()
 def get_procedure_data_from_appointment(patient_appointment):
-
 	idr_appointment_type = frappe.db.get_value("Patient Appointment", patient_appointment, "idr_appointment_type")
 	rate = frappe.db.get_value("Item Price", {"item_code":idr_appointment_type}, "price_list_rate")
 
@@ -37,3 +36,45 @@ def get_procedure_data_from_appointment(patient_appointment):
 	}
 	
 	return out
+
+@frappe.whitelist()
+def physician_after_insert(self, method):
+	#Create Supplier for invoicing
+	supplier = frappe.new_doc("Supplier")
+	supplier.supplier_name = " ".join([self.first_name, self.middle_name, self.last_name])
+	supplier.supplier_type = "Member" if self.is_member else "Non Member"
+	supplier.save()
+
+	self.db_set("idr_supplier", supplier.name)
+
+	#TODO: create tax rules
+	
+@frappe.whitelist()
+def patient_after_insert(self, method):
+	#Create contact and address and link to customer
+	address = frappe.new_doc("Address")
+	address.title = self.patient_name
+	address.address_line1 = self.idr_address_line1
+	address.address_line2 = self.idr_address_line2
+	address.city = self.idr_city
+	addres.pincode = self.idr_pincode
+	address.country = frappe.defaults.get_defaults().get("country")
+	address.append('links', {
+		"link_doctype": "Customer",
+		"link_name": self.customer
+	})
+	address.save()
+
+	contact = frappe.new_doc("Contact")
+	contact.first_name = self.patient_name.split(" ")[0]
+	contact.last_name = self.patient_name.split(" ")[0] or ""
+	address.append('links', {
+		"link_doctype": "Customer",
+		"link_name": self.customer
+	})
+	contact.save()
+
+	frappe.db.commit()
+
+	#Rename Customer
+	frappe.rename_doc("Customer", self.customer, self.patient_name)
