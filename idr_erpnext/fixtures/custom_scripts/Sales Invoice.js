d2597ac9d025646879195e1bbfa7dff64da2e223
Gaurav Naik <gvrvnk@gmail.com>
2018-03-23 12:16:40 +0530

    Removed code for creating invoice from patient

--- idr_erpnext/fixtures/custom_scripts/Sales Invoice.js
+++ idr_erpnext/fixtures/custom_scripts/Sales Invoice.js
@@ -2,11 +2,7 @@ frappe.ui.form.on("Sales Invoice", {
 	"onload_post_render": function(frm) {
 		//Scroll directly to items if Invoice has been opened from Patient doctype.
 		var last_route = frappe.route_history.slice(-2, -1)[0];
-		if (last_route && last_route[0] == "Form" && last_route[1] == "Patient") {
-			frappe.model.add_child(cur_frm.doc, "Sales Invoice Item", "items", 0);
-			refresh_field("items");
- 			frappe.ui.scroll("[data-fieldname='currency']", true);
-		} else if (last_route && last_route[0] == "Form" && last_route[1] == "Patient Appointment") {
+		if (last_route && last_route[0] == "Form" && last_route[1] == "Patient Appointment") {
 
 			frappe.call({
 				method: "idr_erpnext.api.get_procedure_data_from_appointment",
