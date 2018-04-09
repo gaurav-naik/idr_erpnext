ec92597c43e2dcd0e1d8c515fd91be9ed6a429c7
Gaurav Naik <gvrvnk@gmail.com>
2018-04-10 03:29:14 +0530

    [WIP] Autoset physician, available date in Patient Appointment

--- idr_erpnext/fixtures/custom_scripts/Patient Appointment.js
+++ idr_erpnext/fixtures/custom_scripts/Patient Appointment.js
@@ -1,6 +1,31 @@
 frappe.ui.form.on("Patient Appointment", {
+	patient: function(frm) {
+		frappe.call({
+			method: "idr_erpnext.api.get_earliest_available_physician"// get_physician_with_earliest_timeslot"
+		}).done(function(r) {
+			if (!r.exc) {
+				frm.set_value("physician", r.earliest_available_physician);
+			}
+		}).error(function(err) {
+			frappe.show_alert(__("Could not fetch earliest available date."));
+		});
+	},
+	physician: function(frm) {
+		frappe.call({
+			method: "idr_erpnext.api.get_earliest_available_date",
+			args: {
+				"physician": frm.doc.physician
+			}
+		}).done(function(r) {
+			if (!r.exc) {
+				frm.set_value("appointment_date", r.earliest_available_date);
+			}
+		}).error(function(err) {
+			frappe.show_alert(__("Could not fetch earliest available date."));
+		});
+	}
 });
 
-frappe.ui.keys.on("shift+c", function(e) {
-	frappe.click_button("Check availability")
-})
+frappe.ui.keys.on("alt+c", function(e) {
+	frappe.click_button("Check availability");
+});
