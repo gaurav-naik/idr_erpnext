4e19b01f292638c223497be0ffdfa970849c24a3
Gaurav Naik <gvrvnk@gmail.com>
2018-04-15 17:06:08 +0530

    Removed Sales Invoice code from Patient custom script

--- idr_erpnext/fixtures/custom_scripts/Patient.js
+++ idr_erpnext/fixtures/custom_scripts/Patient.js
@@ -19,9 +19,6 @@ frappe.ui.form.on("Patient", {
 			frm.set_value("idr_patient_address", undefined);
 		}
 	},
-	"refresh": function(frm) {
-		add_custom_buttons(frm);
-	},
 	"idr_patient_first_name": function(frm) {
 		make_full_name(frm);
 	},
@@ -50,38 +47,4 @@ function set_patient_address(frm) {
 	} else {
 		frm.set_value("idr_patient_address_display", undefined);
 	}
-}
-
-function add_custom_buttons(frm) {
-	if (!frm.doc.__islocal) {
-		frm.add_custom_button(__('Sales Invoice'), function() {
-			make_sales_invoice(frm);
-		},__("Create"));
-	}
-
-	//Suspended
-	// if (!(__("Generate Codice Fiscale") in frm.custom_buttons)) {
-	// 	frm.add_custom_button(__("Generate Codice Fiscale"), function() {
-	// 		if (!frm.doc.sex) {
-	// 			frappe.msgprint(__("Gender is required for generating Codice Fiscale"));
-	// 		} else if (!frm.doc.idr_place_of_birth) {
-	// 			frappe.msgprint(__("Place of birth is required for generating Codice Fiscale"));
-	// 		} else {
-	// 			frappe.call({
-	// 				method:"idr_erpnext.api.generate_codice_fiscale",
-	// 				args: {
-	// 					"first_name":frm.doc.idr_patient_first_name,
-	// 					"last_name":frm.doc.idr_patient_last_name,
-	// 					"gender": frm.doc.sex,
-	// 					"date_of_birth": frm.doc.dob,
-	// 					"place_of_birth": frm.doc.idr_place_of_birth
-	// 				}
-	// 			}).done(function(r) {
-	// 				if (!r.exc) {
-	// 					frm.set_value("idr_patient_codice_fiscale", r.message);
-	// 				}
-	// 			});
-	// 		}
-	// 	});
-	// }
 }
