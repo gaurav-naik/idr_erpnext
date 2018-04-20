fb64fc60d4582104b9a887537f0d608297bbd208
Gaurav Naik <gvrvnk@gmail.com>
2018-04-20 12:06:42 +0530

    [Fix] Custom button, day-date visibility in Physician schedulee. Issue #11

--- idr_erpnext/fixtures/custom_scripts/Physician Schedule.js
+++ idr_erpnext/fixtures/custom_scripts/Physician Schedule.js
@@ -42,18 +42,40 @@ frappe.ui.form.on("Physician Schedule", {
 			});
 			d.show();
 		});
+		frm.trigger("manage_custom_button_visibility");
+		frm.trigger("manage_date_day_field_visibility");
 	},
 	idr_schedule_type: function(frm) {
-		if (frm.doc.idr_schedule_type == "Day of Week") {
-			frm.fields_dict.time_slots.grid.set_column_disp("idr_date", false);
-			frm.fields_dict.time_slots.grid.set_column_disp("day", true);
-			frm.doc.custom_button["Add Time Slots"].show();
-			frm.doc.custom_button["Add Time Slots by Date"].hide();
-		} else if (frm.doc.idr_schedule_type == "Date") {
+		frm.trigger("manage_custom_button_visibility");
+		frm.trigger("manage_date_day_field_visibility");
+		// if (frm.doc.idr_schedule_type == "Day of Week") {
+		// 	frm.fields_dict.time_slots.grid.set_column_disp("idr_date", false);
+		// 	frm.fields_dict.time_slots.grid.set_column_disp("day", true);
+		// 	frm.doc.custom_button["Add Time Slots"].show();
+		// 	frm.doc.custom_button["Add Time Slots by Date"].hide();
+		// } else if (frm.doc.idr_schedule_type == "Date") {
+		// 	frm.fields_dict.time_slots.grid.set_column_disp("idr_date", true);
+		// 	frm.fields_dict.time_slots.grid.set_column_disp("day", false);
+		// 	frm.doc.custom_button["Add Time Slots"].hide();
+		// 	frm.doc.custom_button["Add Time Slots by Date"].show();
+		// }
+	},
+	manage_custom_button_visibility: function(frm) {
+		if (frm.doc.idr_schedule_type == "Date") {
+			frm.custom_buttons["Add Time Slots"].hide();
+			frm.custom_buttons["Add Time Slots by Date"].show();
+		} else {
+			frm.custom_buttons["Add Time Slots"].show();
+			frm.custom_buttons["Add Time Slots by Date"].hide();
+		}
+	},
+	manage_date_day_field_visibility: function(frm) {
+		if (frm.doc.idr_schedule_type == "Date") {
 			frm.fields_dict.time_slots.grid.set_column_disp("idr_date", true);
 			frm.fields_dict.time_slots.grid.set_column_disp("day", false);
-			frm.doc.custom_button["Add Time Slots"].hide();
-			frm.doc.custom_button["Add Time Slots by Date"].show();
+		} else {
+			frm.fields_dict.time_slots.grid.set_column_disp("idr_date", false);
+			frm.fields_dict.time_slots.grid.set_column_disp("day", true);
 		}
 	}
 });
