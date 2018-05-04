bc5dec03e1d256a74b03341dbd9b70ac63bf7b3b
Gaurav Naik <gvrvnk@gmail.com>
2018-05-04 11:27:06 +0530

    [WIP] Codice Fiscale

--- idr_erpnext/fixtures/custom_scripts/Customer.js
+++ idr_erpnext/fixtures/custom_scripts/Customer.js
@@ -1,4 +1,36 @@
 frappe.ui.form.on("Customer", {
+	refresh: function(frm) {
+		$(".generate-cf").on("click", function() {
+			frappe.call({
+				method: "idr_erpnext.api.generate_codice_fiscale",
+				args: {
+					first_name: frm.doc.idr_customer_first_name, 
+					last_name: frm.doc.idr_customer_last_name, 
+					date_of_birth: frm.doc.idr_customer_date_of_birth, 
+					gender: frm.doc.idr_customer_gender, 
+					place_of_birth: frm.doc.idr_customer_place_of_birth
+				}
+			}).done(function(r) {
+				console.log(r);
+				frm.set_value(r.message);
+			}).error(function(err) {
+				frm.show_alert(__("Unable to set codice fiscale"));
+			});
+		});
+		$(".validate-cf").on("click", function() {
+			frappe.call({
+				method: "idr_erpnext.api.validate_codice_fiscale",
+				args: {
+					codice_fiscale: frm.doc.idr_customer_tax_id, 
+				}
+			}).done(function(r) {
+				console.log(r);
+				frappe.show_alert("Codice Fiscale Verified")
+			}).error(function(err) {
+				frm.show_alert(__("Unable to set codice fiscale"))
+			});
+		});
+	},
 	idr_customer_gender: function(frm) {
 		frm.set_value("gender", frm.doc.idr_customer_gender);
 	},
